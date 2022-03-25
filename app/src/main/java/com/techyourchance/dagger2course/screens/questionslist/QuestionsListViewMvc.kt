0729538ae8d37478824c1e99e45f6e65a1490315
hantash <hantash@sozie.com>
2022-03-26 02:34:50 +0500

    Extraction of UI logic from QuestionList Activity

---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListViewMvc.kt
@@ -0,0 +1,121 @@
+package com.techyourchance.dagger2course.screens.questionslist
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.techyourchance.dagger2course.Constants
+import com.techyourchance.dagger2course.R
+import com.techyourchance.dagger2course.questions.Question
+import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.ArrayList
+
+class QuestionListViewMvc(
+    private val layoutInflater: LayoutInflater,
+    private val parent: ViewGroup?
+) {
+
+    interface Listener {
+        fun onRefreshClicked()
+        fun onQuestionClicked(clickedQuestion: Question)
+    }
+
+    val rootView: View
+    val listeners = HashSet<Listener>()
+
+    private val swipeRefresh: SwipeRefreshLayout
+    private val recyclerView: RecyclerView
+    private val questionsAdapter: QuestionsAdapter
+
+    private val context: Context get() = rootView.context
+
+    init {
+        rootView = layoutInflater.inflate(R.layout.layout_questions_list, parent, false)
+
+        // init pull-down-to-refresh
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        swipeRefresh.setOnRefreshListener {
+            for (listener in listeners) {
+                listener.onRefreshClicked()
+            }
+        }
+
+        // init recycler view
+        recyclerView = findViewById(R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        questionsAdapter = QuestionsAdapter{ clickedQuestion ->
+            for (listener in listeners) {
+                listener.onQuestionClicked(clickedQuestion)
+            }
+        }
+        recyclerView.adapter = questionsAdapter
+    }
+
+    fun <T : View?> findViewById(@IdRes id: Int): T {
+        return rootView.findViewById<T>(id)
+    }
+
+    fun registerListener(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun unregisterListener(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+    fun showProgressIndication() {
+        swipeRefresh.isRefreshing = true
+    }
+
+    fun hideProgressIndication() {
+        if (swipeRefresh.isRefreshing) {
+            swipeRefresh.isRefreshing = false
+        }
+    }
+
+    fun bindQuestions(questions: List<Question>) {
+        questionsAdapter.bindData(questions)
+    }
+
+
+    class QuestionsAdapter(
+        private val onQuestionClickListener: (Question) -> Unit
+    ) : RecyclerView.Adapter<QuestionsAdapter.QuestionViewHolder>() {
+
+        private var questionsList: List<Question> = ArrayList(0)
+
+        inner class QuestionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val title: TextView = view.findViewById(R.id.txt_title)
+        }
+
+        fun bindData(questions: List<Question>) {
+            questionsList = ArrayList(questions)
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_question_list_item, parent, false)
+            return QuestionViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
+            holder.title.text = questionsList[position].title
+            holder.itemView.setOnClickListener {
+                onQuestionClickListener.invoke(questionsList[position])
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return questionsList.size
+        }
+    }
+
+}
